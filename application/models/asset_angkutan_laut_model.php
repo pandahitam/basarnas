472a23a395f5feb3a90d61390e3ed613ac17cc48
Rudiman <rudi1206@gmail.com>
2014-02-11 17:53:01 +0700

    AngkutanLaut Paging has now been corrected. caused by: "LIMIT $start, $limit" statement did not previously exist.

--- application/models/asset_angkutan_laut_model.php
+++ application/models/asset_angkutan_laut_model.php
@@ -184,7 +184,7 @@ class Asset_Angkutan_Laut_Model extends MY_Model{
             {
                 $query = "$this->selectColumn
                                 FROM $this->viewTable
-                                ";
+                                LIMIT $start, $limit";
                 
                 if($searchByBarcode != null)
                 {
